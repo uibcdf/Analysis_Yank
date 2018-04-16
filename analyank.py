315773448ee9308a9697e7cee61f000a23e8d959
Diego Prada <prada.gracia@gmail.com>
2018-04-16 13:42:08 -0500

    iniciando

---
+++ analyank.py
@@ -0,0 +1,16 @@
+import yank
+import numpy as np
+
+def version():
+    print("version 0.0")
+
+def get_REMDtraj(thermodynamic_state=0,object_nc=None):
+    if not object_nc:
+        print("Necesito un objeto nc salido de Yank")
+        return
+
+    print("Hago algo y ahí va")
+    traj=None
+    return traj
+
+
